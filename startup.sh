a7cc9068f2fe02320b254f8b6933a5cdd0c3f9b6
bcrhbrhcdb <apet2804@mpsedu.org>
2025-01-15 11:34:34 -0600

    qewfer

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --get commit.template
+git for-each-ref --format=%(refname)%00%(upstream:short)%00%(objectname)%00%(upstream:track)%00%(upstream:remotename)%00%(upstream:remoteref) refs/heads/next-update refs/remotes/next-update
+git status -z -uall
+git checkout -q --track origin/next-update
